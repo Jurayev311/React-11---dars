a8fc004cabd12aaa6de87fef53982425ce84d990
Javohir708 <joxauzking@gamil.com>
2025-02-02 15:08:43 +0500

    products qismi tayyor

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -24,10 +24,10 @@ const Products = () => {
       <div className="container mx-auto">
         <div className="mb-12 text-[#737373] text-center">
           <h2 className="mb-2.5 text-[20px] font-normal">Featured Products</h2>
-          <h3 className="mb-2.5 text-[24px] font-bold text-[#252B42]">
+          <h3 className="mb-2.5 text-[24px] sm:text-[28px] font-bold text-[#252B42]">
             BESTSELLER PRODUCTS
           </h3>
-          <p className="text-[14px] font-normal">
+          <p className="text-[14px] sm:text-[16px] font-normal">
             Problems trying to resolve the conflict between
           </p>
         </div>
@@ -50,23 +50,23 @@ const Products = () => {
                 <div className="p-4 text-center">
                   <h2
                     title={product.title}
-                    className="text-[#252B42] text-[16px] font-bold mb-2 truncate"
+                    className="text-[#252B42] text-[16px] sm:text-[18px] font-bold mb-2 truncate"
                   >
                     {product.title}
                   </h2>
-                  <p className="text-[#737373] text-[14px] font-bold mb-2">
+                  <p className="text-[#737373] text-[14px] sm:text-[16px] font-bold mb-2">
                     English Department
                   </p>
-                  <strong className="block text-[#23856D] text-[16px] font-bold mb-3">
+                  <strong className="block text-[#23856D] text-[16px] sm:text-[18px] font-bold mb-3">
                     ${product.price}
                   </strong>
 
-                  <div className="flex items-center justify-center gap-2">
+                  <div className="flex items-center justify-center gap-2 mt-2">
                     {colors.map((color, index) => (
                       <span
                         key={index}
                         style={{ background: color }}
-                        className="w-5 h-5 inline-block rounded-full cursor-pointer border border-gray-300"
+                        className="w-5 h-5 sm:w-6 sm:h-6 inline-block rounded-full cursor-pointer border border-gray-300"
                       ></span>
                     ))}
                   </div>
